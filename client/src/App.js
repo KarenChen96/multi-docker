79a8dd0484321720eabb213847bf1385d96e1846
hp <hp@chenchuns-MacBook-Pro.local>
2019-04-07 14:50:35 -0700

    modify display info

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
+            <h1 className="App-title">Fib Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
